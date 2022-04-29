1f3fb4d34a0ed0a547c0a3f948341ebc0ecdeebe
AmanBhanse <amanbhanse22@gmail.com>
2022-04-29 19:40:56 +0530

    introduce variable for boundaries

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
+const NO_BOUNDARIES = 5;
+
 let ray;
-let boundarys= [];
+let boundaries= [];
 let sun;
 let xOff = 0;
 let yOff = 1000;
@@ -8,31 +10,31 @@ let yOff = 1000;
 function setup() {
   createCanvas(400, 400);
 
-  for(let i=0 ; i<5 ; i++ ){
+  for(let i=0 ; i<NO_BOUNDARIES ; i++ ){
     let x1 = random(width);
     let x2 = random(width);
     let y1 = random(height);
     let y2 = random(height);
-    boundarys[i] = new Boundary(x1, y1 , x2, y2);
+    boundaries[i] = new Boundary(x1, y1 , x2, y2);
   }
 
   //inclosing the canvas with boundaries
-  boundarys.push(new Boundary(0,0,0,width));
-  boundarys.push(new Boundary(0,width,width, height));
-  boundarys.push(new Boundary(width,height,height,0));
-  boundarys.push(new Boundary(height,0,0,0));
+  boundaries.push(new Boundary(0,0,0,width));
+  boundaries.push(new Boundary(0,width,width, height));
+  boundaries.push(new Boundary(width,height,height,0));
+  boundaries.push(new Boundary(height,0,0,0));
 
   sun = new Sun();
 }
 
 function draw() {
   background(0);
-  for(wall of boundarys){
+  for(wall of boundaries){
     wall.show();
   }
   sun.updatePostion(noise(xOff) * width , noise(yOff)*height);
   sun.show();
-  sun.throwLightOnObject(boundarys);
+  sun.throwLightOnObject(boundaries);
   
   
   xOff += 0.01;
